75560730c689d03b9f01216cf0166e7239c180c1
Var3n <98960122+DRT1030@users.noreply.github.com>
2024-11-18 09:17:42 +0700

    Co-authored-by: 2000onetechguy@gmail.com <2000onetechguy@gmail.com>

--- js/r3gdialogs.js
+++ js/r3gdialogs.js
@@ -6,14 +6,51 @@ const reduceO = gebi("openReduce");
 const reuseO = gebi("openReuse");
 const recycleO = gebi("openRecycle");
 
-reduceO.onclick = function () {
-    reduceD.showModal();
+function onCloseAnyAll() {
+    console.log("close");
+    document.body.style.overflow = "initial";
+    window.onclick = null;
+
 }
 
-reuseO.onclick = function () {
-    reuseD.showModal();
+function onOpenAnyAll() {
+    document.body.style.overflow = "hidden";
 }
 
-recycleO.onclick = function () {
-    recycleD.showModal();
+function genOpener(dialog2open, others){
+    
+    return function () {
+        for(const d of others){
+            d.close();
+        }
+
+        onOpenAnyAll();
+        dialog2open.showModal();
+
+        let opened = true;
+        window.onclick = function() {
+            if(opened) {
+                opened = false;
+                return;
+            }
+            console.log("click")
+            for(const d of [reduceD, reuseD, recycleD]) {
+                d.close();
+            }
+        }
+    }
 }
+
+reduceO.onclick = genOpener(reduceD, [reuseD, recycleD]);
+
+reuseO.onclick = genOpener(reuseD, [reduceD, recycleD]);
+
+recycleO.onclick = genOpener(recycleD, [reduceD, reuseD]);
+
+
+for(const d of [reduceD, reuseD, recycleD]){
+    d.onClose = onCloseAnyAll;
+    d.style.pointerEvents = "none";
+    d.onclick = d.onClose;
+}
+
